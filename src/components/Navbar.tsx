d01e102879cc68a3f91fa0d953fa63eb265c5c4c
feyzanursener <senerffeyza@gmail.com>
2026-07-03 14:52:47 +0300

    logo eklendi

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,8 +16,12 @@ export default function Navbar() {
         <div className="flex items-center justify-between h-16">
           {/* Logo / Marka */}
           <div className="flex-shrink-0">
-            <Link href="/" className="text-xl font-black tracking-tight text-indigo-600 hover:text-indigo-700 transition">
-              Multiacademy
+            <Link href="/" className="flex items-center gap-2">
+              <img 
+                src="/images/store-logo.jpg" 
+                alt="MultiGroup Store" 
+                className="h-10 w-auto"
+              />
             </Link>
           </div>
 
