44898b6fa15f7b260a9082c36c832d7943ef4a00
feyzanursener <senerffeyza@gmail.com>
2026-07-03 17:35:32 +0300

    öne çıkan ürünler slider yapısı içine alındı

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -90,48 +90,49 @@ if (categoryCheck.count === 0) {
   // Aksesuar Ürünleri (category_id: 1)
   insertProduct.run('Şapka - Siyah', 'Şık ve rahat günlük kullanım için siyah şapka', 299.99, 25, '/images/MultiGroup-şapka-black.jfif', cat1.lastInsertRowid, 1);
 
-  insertProduct.run('Şapka - Lacivert', 'Şık ve rahat günlük kullanım için lacivert şapka', 299.99, 25, '/images/MultiGroup-şapka-laci.jfif', cat1.lastInsertRowid, 1);
+  insertProduct.run('Şapka - Lacivert', 'Şık ve rahat günlük kullanım için lacivert şapka', 299.99, 25, '/images/MultiGroup-şapka-laci.jfif', cat1.lastInsertRowid, 0);
 
-  insertProduct.run('Şapka - Kırmızı', 'Şık ve rahat günlük kullanım için kırmızı şapka', 299.99, 25, '/images/MultiGroup-şapka-kırmızı.png', cat1.lastInsertRowid, 1);
+  insertProduct.run('Şapka - Kırmızı', 'Şık ve rahat günlük kullanım için kırmızı şapka', 299.99, 25, '/images/MultiGroup-şapka-kırmızı.png', cat1.lastInsertRowid, 0);
 
-  insertProduct.run('Şapka - Bej', 'Şık ve rahat günlük kullanım için bej şapka', 299.99, 25, '/images/MultiGroup-şapka-bej.jfif', cat1.lastInsertRowid, 1);
+  insertProduct.run('Şapka - Bej', 'Şık ve rahat günlük kullanım için bej şapka', 299.99, 25, '/images/MultiGroup-şapka-bej.jfif', cat1.lastInsertRowid, 0);
 
-  insertProduct.run('Bez Çanta - Siyah', 'Dayanıklı kumaş, geniş iç hacimli bez çanta', 449.90, 30, '/images/MultiGroup-bez çanta.png', cat1.lastInsertRowid, 1);
+  insertProduct.run('Bez Çanta - Siyah', 'Dayanıklı kumaş, geniş iç hacimli bez çanta', 449.90, 30, '/images/MultiGroup-bez çanta.png', cat1.lastInsertRowid, 0);
 
-  insertProduct.run('Bez Çanta - Lacivert', 'Dayanıklı kumaş, geniş iç hacimli bez çanta', 449.90, 30, '/images/bez çanta-laci.png', cat1.lastInsertRowid, 1);
+  insertProduct.run('Bez Çanta - Lacivert', 'Dayanıklı kumaş, geniş iç hacimli bez çanta', 449.90, 30, '/images/bez çanta-laci.png', cat1.lastInsertRowid, 0);
 
   // Giyim Ürünleri (category_id: 2)
   insertProduct.run('Hoodie - Siyah', 'Rahat kesim, kaliteli kumaş siyah hoodie', 599.99, 40, '/images/MultiGroup-hoodie-black.png', cat2.lastInsertRowid, 1);
 
-  insertProduct.run('Hoodie - Beyaz', 'Rahat kesim, kaliteli kumaş beyaz hoodie', 599.99, 40, '/images/hoodie-white.png', cat2.lastInsertRowid, 1);
+  insertProduct.run('Hoodie - Beyaz', 'Rahat kesim, kaliteli kumaş beyaz hoodie', 599.99, 40, '/images/hoodie-white.png', cat2.lastInsertRowid, 0);
 
   insertProduct.run('T-Shirt - Siyah', '%100 pamuk, oversize kesim siyah tişört', 349.99, 50, '/images/MultiGroup-tshirt-black.png', cat2.lastInsertRowid, 1);
 
-  insertProduct.run('T-Shirt - Beyaz', '%100 pamuk, oversize kesim beyaz tişört', 349.99, 50, '/images/MultiGroup-tshirt-whitej.png', cat2.lastInsertRowid, 1);
+  insertProduct.run('T-Shirt - Beyaz', '%100 pamuk, oversize kesim beyaz tişört', 349.99, 50, '/images/MultiGroup-tshirt-whitej.png', cat2.lastInsertRowid, 0);
 
-  insertProduct.run('T-Shirt - Kırmızı', '%100 pamuk, oversize kesim kırmızı tişört', 349.99, 50, '/images/MultiGroup-tshirt-red.png', cat2.lastInsertRowid, 1);
+  insertProduct.run('T-Shirt - Kırmızı', '%100 pamuk, oversize kesim kırmızı tişört', 349.99, 50, '/images/MultiGroup-tshirt-red.png', cat2.lastInsertRowid, 0);
 
   
 
   // Teknoloji Ürünleri (category_id: 3)
-  insertProduct.run('Powerbank', '20000mAh kapasiteli hızlı şarj powerbank', 699.99, 35, '/images/MultiGroup-powerbank.png', cat3.lastInsertRowid, 1);
+  insertProduct.run('Powerbank', '20000mAh kapasiteli hızlı şarj powerbank', 699.99, 35, '/images/MultiGroup-powerbank.png', cat3.lastInsertRowid, 0);
+
   insertProduct.run('Mousepad', 'Büyük boy, anti-kaymaz yüzey mousepad', 199.99, 45, '/images/MultiGroup-mousepad.png', cat3.lastInsertRowid, 1);
 
   // Kırtasiye Ürünleri (category_id: 4)
   insertProduct.run('Defter - Siyah', 'Kraft kapaklı, 100 yaprak kaliteli defter', 149.99, 60, '/images/MultiGroup-defter.jfif', cat4.lastInsertRowid, 1);
 
-  insertProduct.run('Defter - Beyaz', 'Kraft kapaklı, 100 yaprak kaliteli defter', 149.99, 60, '/images/defter-white.png', cat4.lastInsertRowid, 1);
+  insertProduct.run('Defter - Beyaz', 'Kraft kapaklı, 100 yaprak kaliteli defter', 149.99, 60, '/images/defter-white.png', cat4.lastInsertRowid, 0);
 
-  insertProduct.run('Masa Takvimi', '2024 masa takvimi, şık tasarım', 89.99, 40, '/images/MultiGroup-masa takvimi.png', cat4.lastInsertRowid, 1);
+  insertProduct.run('Masa Takvimi', '2024 masa takvimi, şık tasarım', 89.99, 40, '/images/MultiGroup-masa takvimi.png', cat4.lastInsertRowid, 0);
 
   // Yaşam Ürünleri (category_id: 5)
   insertProduct.run('Termos - Siyah', '500ml kapasiteli, uzun süre sıcak tutan termos', 399.99, 25, '/images/MultiGroup-termos-black.jfif', cat5.lastInsertRowid, 1);
 
-  insertProduct.run('Termos - Beyaz', '500ml kapasiteli, uzun süre sıcak tutan termos', 399.99, 25, '/images/termos-white.png', cat5.lastInsertRowid, 1);
+  insertProduct.run('Termos - Beyaz', '500ml kapasiteli, uzun süre sıcak tutan termos', 399.99, 25, '/images/termos-white.png', cat5.lastInsertRowid, 0);
 
   insertProduct.run('Küçük Termos - Siyah', '350ml kapasiteli, taşınabilir termos', 299.99, 30, '/images/MultiGroup-termos-küçük.png', cat5.lastInsertRowid, 1);
 
-  insertProduct.run('Küçük Termos - Kırmızı', '350ml kapasiteli, taşınabilir termos', 299.99, 30, '/images/kucuk-termos-red.png', cat5.lastInsertRowid, 1);
+  insertProduct.run('Küçük Termos - Kırmızı', '350ml kapasiteli, taşınabilir termos', 299.99, 30, '/images/kucuk-termos-red.png', cat5.lastInsertRowid, 0);
 
   insertProduct.run('Kupa - Siyah', 'Seramik mat siyah kupa', 149.99, 50, '/images/MultiGroup-kupa-black.png', cat5.lastInsertRowid, 1);
 
